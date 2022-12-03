7c5063d6035f61a8add8fc1767eeeedd5f99aa62
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-12-03 23:34:07 +0300

    addall

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,6 @@
 import Notiflix from 'notiflix';
+import { listOfCountriesRef, boxForCountry } from './index';
+
 Notiflix.Notify.init({
     borderRadius: '10px',
     position: 'center-center',
@@ -18,15 +20,55 @@ export function fetchCountries(name) {
         .then(response => { return response.json() })
         .then(countryInf => {
             if (countryInf.length > 10) {
-               Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.'); 
-            }
-            console.dir(countryInf);
-            console.log(countryInf.length);
+                clearContent();
+                return Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+            };
+            if (countryInf.length >= 2 && countryInf.length <= 10) {
+                boxForCountry.innerHTML = '';
+
+               return createListForCountries(countryInf);
+            };
+           
+            if (countryInf.length = 1) {
+                clearContent();
+                return createCountry(countryInf);
+            };
+            if (!countryInf.length) {
+                clearContent();
+            };
+
         })
+        .catch((error) => {
+            console.log(error);
+             Notiflix.Notify.failure('Oops, there is no country with that name');
+        });
 };
-// https://restcountries.com/v2/{service}?fields={name.official},{capital},{population},{flags.svg},{languages}
-// name.official - повна назва країни
-// capital - столиця
-// population - населення
-// flags.svg - посилання на зображення прапора
-// languages - масив мов
+
+function createCountry(countryInf) { 
+return boxForCountry.insertAdjacentHTML('afterbegin',
+                     `<div class ="countryFlagBox">
+                <img class = "imgFlagOfCountry" src="${countryInf[0].flag}" alt="flag of ${countryInf[0].name}" />
+                 <h2 class = "titleOfCountry">${countryInf[0].name}</h2>
+                 </div>
+                    <ul class = "countryList">
+                      <li><p><span class = "nameOfList">Capital:</span>${countryInf[0].capital}</p></li>
+                      <li><p><span class = "nameOfList">Population:</span>${countryInf[0].population}</p></li>
+                      <li><p><span class = "nameOfList">Langueges:</span>${countryInf[0].languages[0].name}</p></li>
+                    </ul>`);
+};
+
+function createListForCountries(countryInf) { 
+ const elementCountries = countryInf.map((country) =>
+    `<li>
+    <div class ="countryFlagBox">
+    <img class = "imgFlagOfCountry" src="${country.flag}" alt="flag of ${country.name}">
+    <p>${country.name}</p>
+    </div>
+    </li>`).join("");
+    return listOfCountriesRef.insertAdjacentHTML('afterbegin', elementCountries);
+};
+
+function clearContent() {
+    boxForCountry.innerHTML = '';
+    listOfCountriesRef.innerHTML = '';
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 let debounce = require('lodash.debounce');
 import './css/styles.css';
-import {fetchCountries} from './fetchCountries'
+import { fetchCountries } from './fetchCountries'
+
 const DEBOUNCE_DELAY = 300;
 const inputCountryRef = document.querySelector('#search-box');
+export const listOfCountriesRef = document.querySelector('.country-list');
+export const boxForCountry = document.querySelector('.country-info');
 inputCountryRef.addEventListener('input',debounce(e => {
     let name = e.target.value;
     console.log(name);
     fetchCountries(`${name}`);
 
-},300) );
+},DEBOUNCE_DELAY) );
