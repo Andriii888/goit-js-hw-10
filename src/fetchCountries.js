40752308f488a7bf8129bd623e17afef57acac40
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-12-04 22:10:00 +0300

    addpravka

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,74 +1,12 @@
-import Notiflix from 'notiflix';
-import { listOfCountriesRef, boxForCountry } from './index';
 
-Notiflix.Notify.init({
-    borderRadius: '10px',
-    position: 'center-center',
-     warning: {
-    background: '#eebf31',
-    textColor: 'black',
-    childClassName: 'notiflix-notify-warning',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-exclamation-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(238,191,49,0.2)',
-  },
-});
-export function fetchCountries(name) {
-    console.log(name);
-    fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`)
-        .then(response => { return response.json() })
-        .then(countryInf => {
-            if (countryInf.length > 10) {
-                clearContent();
-                return Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
-            };
-            if (countryInf.length >= 2 && countryInf.length <= 10) {
-                boxForCountry.innerHTML = '';
-
-               return createListForCountries(countryInf);
-            };
-           
-            if (countryInf.length = 1) {
-                clearContent();
-                return createCountry(countryInf);
-            };
-            if (!countryInf.length) {
-                clearContent();
-            };
-
-        })
-        .catch((error) => {
-            console.log(error);
-             Notiflix.Notify.failure('Oops, there is no country with that name');
-        });
+export function fetchCountries(val) {
+   return fetch(`https://restcountries.com/v2/name/${val}?fields=name,capital,population,flag,languages`)
+       .then(response => {
+           if (!response.ok) {
+       throw new Error(response.status);
+           }
+           return response.json()
+       })
+        
 };
 
-function createCountry(countryInf) { 
-return boxForCountry.insertAdjacentHTML('afterbegin',
-                     `<div class ="countryFlagBox">
-                <img class = "imgFlagOfCountry" src="${countryInf[0].flag}" alt="flag of ${countryInf[0].name}" />
-                 <h2 class = "titleOfCountry">${countryInf[0].name}</h2>
-                 </div>
-                    <ul class = "countryList">
-                      <li><p><span class = "nameOfList">Capital:</span>${countryInf[0].capital}</p></li>
-                      <li><p><span class = "nameOfList">Population:</span>${countryInf[0].population}</p></li>
-                      <li><p><span class = "nameOfList">Langueges:</span>${countryInf[0].languages[0].name}</p></li>
-                    </ul>`);
-};
-
-function createListForCountries(countryInf) { 
- const elementCountries = countryInf.map((country) =>
-    `<li>
-    <div class ="countryFlagBox">
-    <img class = "imgFlagOfCountry" src="${country.flag}" alt="flag of ${country.name}">
-    <p>${country.name}</p>
-    </div>
-    </li>`).join("");
-    return listOfCountriesRef.insertAdjacentHTML('afterbegin', elementCountries);
-};
-
-function clearContent() {
-    boxForCountry.innerHTML = '';
-    listOfCountriesRef.innerHTML = '';
-}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,81 @@
 let debounce = require('lodash.debounce');
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries'
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+    borderRadius: '10px',
+    position: 'center-center',
+     warning: {
+    background: '#eebf31',
+    textColor: 'black',
+    childClassName: 'notiflix-notify-warning',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-exclamation-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(238,191,49,0.2)',
+  },
+});
 
 const DEBOUNCE_DELAY = 300;
 const inputCountryRef = document.querySelector('#search-box');
-export const listOfCountriesRef = document.querySelector('.country-list');
-export const boxForCountry = document.querySelector('.country-info');
+const listOfCountriesRef = document.querySelector('.country-list');
+const boxForCountry = document.querySelector('.country-info');
+
 inputCountryRef.addEventListener('input',debounce(e => {
     let name = e.target.value;
-    console.log(name);
-    fetchCountries(`${name}`);
+        fetchCountries(name).then(countryInf => {
+            if (countryInf.length > 10) {
+                clearContent();
+                return Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+            };
+            if (countryInf.length >= 2 && countryInf.length <= 10) {
+                boxForCountry.innerHTML = '';
 
-},DEBOUNCE_DELAY) );
+               return createListForCountries(countryInf);
+            };
+           
+            if (countryInf.length = 1) {
+                clearContent();
+                return createCountry(countryInf);
+            };
+            if (!countryInf.length) {
+                clearContent();
+            };
+
+        })
+        .catch((error) => {
+            console.log(error);
+             Notiflix.Notify.failure('Oops, there is no country with that name');
+        });
+
+}, DEBOUNCE_DELAY));
+
+function createCountry(val) { 
+return boxForCountry.insertAdjacentHTML('afterbegin',
+                     `<div class ="countryFlagBox">
+                <img class = "imgFlagOfCountry" src="${val[0].flag}" alt="flag of ${val[0].name}" />
+                 <h2 class = "titleOfCountry">${val[0].name}</h2>
+                 </div>
+                    <ul class = "countryList">
+                      <li><p><span class = "nameOfList">Capital:</span>${val[0].capital}</p></li>
+                      <li><p><span class = "nameOfList">Population:</span>${val[0].population}</p></li>
+                      <li><p><span class = "nameOfList">Langueges:</span>${val[0].languages[0].name}</p></li>
+                    </ul>`);
+};
+
+function createListForCountries(val) { 
+ const elementCountries = val.map((country) =>
+    `<li>
+    <div class ="countryFlagBox">
+    <img class = "imgFlagOfCountry" src="${country.flag}" alt="flag of ${country.name}">
+    <p>${country.name}</p>
+    </div>
+    </li>`).join("");
+    return listOfCountriesRef.insertAdjacentHTML('afterbegin', elementCountries);
+};
+
+function clearContent() {
+    boxForCountry.innerHTML = '';
+    listOfCountriesRef.innerHTML = '';
+}
